3796bff4e4a9f2cd6ffe005614a25b8e6938e7ea
Jérôme Clament-Sanz <j.clamentsanz@gmail.com>
2021-04-28 09:08:49 +0200

    remove Android kotlin extension (deprecated)

--- app/src/main/java/com/exoplayer/fullscreen/MainActivity.kt
+++ app/src/main/java/com/exoplayer/fullscreen/MainActivity.kt
@@ -1,19 +1,20 @@
 package com.exoplayer.fullscreen
 
 import android.os.Bundle
-import android.provider.MediaStore
 import androidx.appcompat.app.AppCompatActivity
-import com.google.android.exoplayer2.*
+import com.google.android.exoplayer2.ui.PlayerView
 import com.norulab.exofullscreen.MediaPlayer
 import com.norulab.exofullscreen.preparePlayer
 import com.norulab.exofullscreen.setSource
-import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val playerView = findViewById<PlayerView>(R.id.playerView)
+
         MediaPlayer.initialize(applicationContext)
         MediaPlayer.exoPlayer?.preparePlayer(playerView, true)
         MediaPlayer.exoPlayer?.setSource(applicationContext, "http://html5videoformatconverter.com/data/images/happyfit2.mp4")
